e5b4260e6f7117d0bd8cc95889efc44b40b5696e
akzhol0 <ak.akzhol1603@gmail.com>
2025-06-26 23:55:33 +0500

    created

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
